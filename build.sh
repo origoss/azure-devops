936275b6e704df03ad62d5240c4316054ac56fdd
Valko Laszlo <valko@linux.karinthy.hu>
2023-10-19 11:23:25 +0200

    change to RH UBI8 base image implement non-root agent

--- build.sh
+++ build.sh
@@ -3,19 +3,26 @@
 scriptname=$(readlink -f "$0")
 dirname=$(dirname "$scriptname")
 
+. "$dirname/env.build.sh"
+
+buildname=linuxagent:latest
+
 remoterepo="${remoterepo:-linuxagent}"
 remotetag="${remotetag:-latest}"
+[ -n "$remotehost" ] && remotehost="${remotehost}/"
+[ -n "$remoteprefix" ] && remoteprefix="${remoteprefix}/"
+remotename="${remotehost}${remoteprefix}${remoterepo}:${remotetag}"
 
-remotename="${remotehost}${remoteprefix}/${remoterepo}:${remotetag}"
-
-cmd=$(command -v buildah)
+#cmd=$(command -v buildah)
 [ -z "$cmd" ] && cmd=$(command -v docker)
 
 cd "$dirname" || exit 1
 echo "Building image"
-"$cmd" build -t linuxagent:latest src || exit 1
+"$cmd" build "${build_args[@]}" -t "$buildname" src "$@" || exit 1
 if [ -n "$remotehost" ]; then
   echo "Pushing image to $remotename"
-  "$cmd" tag linuxagent:latest "$remotename"
+  if [ "$remotename" != "$buildname" ]; then
+    "$cmd" tag "$buildname" "$remotename"
+  fi
   "$cmd" push "$remotename"
 fi
--- compose.sh
+++ compose.sh
@@ -5,4 +5,22 @@ dirname=$(dirname "$scriptname")
 
 . "$dirname/env.sh"
 
-cd "$dirname" && docker compose "$@"
+buildname=linuxagent:latest
+
+remoterepo="${remoterepo:-linuxagent}"
+remotetag="${remotetag:-latest}"
+[ -n "$remotehost" ] && remotehost="${remotehost}/"
+[ -n "$remoteprefix" ] && remoteprefix="${remoteprefix}/"
+remotename="${remotehost}${remoteprefix}${remoterepo}:${remotetag}"
+
+export IMAGE="${remotename}"
+
+cmd=()
+
+compose=$(command -v docker-compose)
+if [ -z "$compose" ]; then
+  cmd+=($(command -v docker) compose)
+else
+  cmd+=("$compose")
+fi
+cd "$dirname" && "${cmd[@]}" "$@"
